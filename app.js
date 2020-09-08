2196c665a03a92934f67c1fc903445970d8b139f
Stephen Scholtz <scholtz.gnome@gmail.com>
2020-09-08 10:51:15 +0200

    added get and listen in app.js for server

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.get();
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.listen(3000, () => {
+  console.log(" - - Listening for requests on port 3000 - - ");
+});
