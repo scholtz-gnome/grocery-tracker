2356f77d510719b95b0ebef5273b4f2073d1c84e
Stephen Scholtz <scholtz.gnome@gmail.com>
2020-09-08 15:26:19 +0200

    added template views and partials. also created static folder for public directory

--- README.md
+++ README.md
@@ -4,10 +4,11 @@ An app made with HTML, CSS and JS. Tracks your grocery items like a typical to-d
 # getting node_modules
 Run | npm init | in your console (assuming Node.js is already installed)
 
-This should install the following libraries: express and nodemon
+This should install the following libraries: ejs, express and nodemon
 
 In your package.json file, under dependencies, you should have:
 "dependencies": {
+  "ejs": "^3.1.5",
   "express": "^4.17.1",
   "nodemon": "^2.0.4"
 }
--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 const express = require("express");
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello world!");
-});
+// set the template view engine
+app.set("view engine", "ejs");
+// app.set(express.static("public"));
 
 app.listen(3000, () => {
   console.log(" - - Listening for requests on port 3000 - - ");
+});
+
+// static files
+app.use(express.static("public"));
+
+//routes
+// HOME PAGE
+app.get("/", (req, res) => {
+  res.render("index", { title: "HOME" });
+});
+
+// ABOUT PAGE
+app.get("/about", (req, res) => {
+  res.render("about", { title: "ABOUT" });
+});
+
+// USER PAGE
+app.get("/user", (req, res) => {
+  res.render("user", { title: "USER" });
+});
+
+// 404 PAGE
+app.use((req, res) => {
+  res.status(404).render("404", { title: "ERROR - 404" });
 });
