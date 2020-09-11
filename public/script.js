8204530d9fbff685047ee128c9a2c8fc2773c7c7
Stephen Scholtz <scholtz.gnome@gmail.com>
2020-09-11 16:24:06 +0200

    neatened up script.js in public

--- public/script.js
+++ public/script.js
@@ -1,7 +1,22 @@
 const deleteButton = document.querySelector("button.delete");
+const deleteItems = document.querySelectorAll("button.delete-item");
 
-  deleteButton.addEventListener("click", () => {
-    const endpoint = `/lists/edit/${deleteButton.dataset.doc}`;
+deleteButton.addEventListener("click", () => {
+  const endpoint = `/lists/edit/${deleteButton.dataset.doc}`;
+
+  fetch(endpoint, {
+    method: "DELETE"
+  })
+  .then(response => response.json())
+  .then(data => window.location.href = data.redirect)
+  .catch(err => console.log(err));
+});
+
+deleteItems.forEach(item => {
+  item.addEventListener("click", () => {
+    alert(`Are you sure you want to remove ${item.dataset.doc}?`);
+
+    const endpoint = `/lists/edit-item/${item.dataset.id}/${item.dataset.doc}`;
 
     fetch(endpoint, {
       method: "DELETE"
@@ -10,19 +25,4 @@ const deleteButton = document.querySelector("button.delete");
     .then(data => window.location.href = data.redirect)
     .catch(err => console.log(err));
   });
-
-  const deleteItems = document.querySelectorAll("button.delete-item");
-  deleteItems.forEach(item => {
-    item.addEventListener("click", () => {
-      alert(`Are you sure you want to remove ${item.dataset.doc}?`);
-
-      const endpoint = `/lists/edit-item/${item.dataset.id}/${item.dataset.doc}`;
-
-      fetch(endpoint, {
-        method: "DELETE"
-      })
-      .then(response => response.json())
-      .then(data => window.location.href = data.redirect)
-      .catch(err => console.log(err));
-    });
-  });
+});
