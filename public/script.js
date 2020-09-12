af43a83535f40cc77ed5a6aeec1970cbf4de6954
Stephen Scholtz <scholtz.gnome@gmail.com>
2020-09-12 12:03:13 +0200

    simplified the syling on the list-items list; trying to stick with flexbox for simplicity

--- public/script.js
+++ public/script.js
@@ -1,25 +1,18 @@
 const deleteList = document.querySelector("button.deleteList"); //get reference to delete list button
 const trashcans = document.querySelectorAll("button.trashcan"); //get reference to trashcan icon on each list item
 
-const iconOne = document.querySelectorAll(".fas");
+const iconsOne = document.querySelectorAll(".fas");
 const iconTwo = document.querySelectorAll(".far");
 
 const invisDivs = document.querySelectorAll(".options");
-const divButtons = document.querySelectorAll(".options");
 
-iconOne.forEach(icon => {
+iconsOne.forEach(icon => {
   icon.addEventListener("click", () => {
     invisDivs.forEach(div => {
       if (div.dataset.item === icon.dataset.item) {
         div.classList.toggle("hide");
       }
     });
-  });
-});
-
-iconTwo.forEach(li => {
-  li.addEventListener("click", (e) => {
-    console.log(li);
   });
 });
 
@@ -32,11 +25,12 @@ deleteList.addEventListener("click", () => {
   .then(response => response.json())
   .then(data => window.location.href = data.redirect)
   .catch(err => console.log(err));
+
 });
 
 trashcans.forEach(trashcan => {
   trashcan.addEventListener("click", () => {
-    const endpoint = `/lists/edit-item/${trashcan.dataset.id}/${trashcan.dataset.doc}`;
+    const endpoint = `/lists/edit-item/${trashcan.dataset.id}/${trashcan.dataset.item}`;
 
     fetch(endpoint, {
       method: "DELETE"
@@ -44,5 +38,6 @@ trashcans.forEach(trashcan => {
     .then(response => response.json())
     .then(data => window.location.href = data.redirect)
     .catch(err => console.log(err));
-    });
+
   });
+});
